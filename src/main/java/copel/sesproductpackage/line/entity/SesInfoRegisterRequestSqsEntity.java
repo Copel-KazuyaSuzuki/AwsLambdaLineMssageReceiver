0b4627f8affe7caed1cb3949dcca5c2455298cde
Copel-KazuyaSuzuki <kazuya.suzuki@copel.jp>
2025-03-15 16:18:32 +0900

    JSON文字列のミスを修正

--- src/main/java/copel/sesproductpackage/line/entity/SesInfoRegisterRequestSqsEntity.java
+++ src/main/java/copel/sesproductpackage/line/entity/SesInfoRegisterRequestSqsEntity.java
@@ -51,15 +51,15 @@ public class SesInfoRegisterRequestSqsEntity extends SQSEntityBase {
     @Override
     protected String getMessageBody() {
         String body = "{"
-                + "\"request_type\": \"" + this.requestType + "\""
-                + "\"from_group\": \"" + this.fromGroup + "\""
-                + "\"from_id\": \"" + this.fromId + "\""
-                + "\"from_name\": \"" + this.fromName + "\"";
+                + "\"request_type\": \"" + this.requestType + "\","
+                + "\"from_group\": \"" + this.fromGroup + "\","
+                + "\"from_id\": \"" + this.fromId + "\","
+                + "\"from_name\": \"" + this.fromName + "\",";
         if (this.rawContent != null) {
         	body += "\"raw_content\": \"" + this.rawContent + "\"";
         }
         if (this.fileId != null) {
-        	body += "\"file_id\": \"" + this.fileId + "\"";
+        	body += "\"file_id\": \"" + this.fileId + "\",";
         }
         if (this.fileName != null) {
         	body += "\"file_name\": \"" + this.fileName + "\"";
